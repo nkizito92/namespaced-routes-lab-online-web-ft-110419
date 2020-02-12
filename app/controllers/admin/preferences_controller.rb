3f8ee3c43a2f1612f2964a616a54091b77198ce2
nkizito92 <nkizito57@gmail.com>
2020-02-11 22:37:49 -0600

    Done.

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,16 +1,15 @@
 class Admin::PreferencesController < ApplicationController
 
     def index 
-        render :index
-        # if params[:artist_id]
-        #     @artist = Artist.find_by(id: params[:artist_id])
-        #     if @artist.nil?
-        #       redirect_to artists_path, alert: "Artist not found"
-        #     else
-        #       @songs = @artist.songs
-        #     end
-        #   else
-        #     @songs = Song.all
-        #   end
+        if params[:artist_id]
+            @artist = Artist.find_by(id: params[:artist_id])
+            if @artist.nil?
+              redirect_to artists_path, alert: "Artist not found"
+            else
+              @songs = @artist.songs
+            end
+          else
+            @songs = Song.all
+          end
     end 
 end 
